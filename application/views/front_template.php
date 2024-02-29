2fcda254c9bb1f7d8b7e24e148f5c29c7028c446
Akhil Kumar <akhil.srikakolapu@gmail.com>
2024-02-29 23:42:59 +0530

    frontend setup
    frontend setup

--- application/views/front_template.php
+++ application/views/front_template.php
@@ -1,22 +1,16 @@
 <?PHP if ( ! defined('BASEPATH')) exit('No direct script access allowed'); 
 	//header of template
-	$this->load->view('layout/header');
+	$this->load->view('layout/front_header');
 ?>
 <?php
 //sidebar of the template
 $this->load->view('layout/sidebar');
 ?>
 <!-- Content Wrapper. Contains page content -->
-<div class="main-content">
-	<div class="page-content">
-        <div class="container-fluid">
-<?php
-$this->load->view('layout/breadcrumbs');
-?>
 	<?php
 		echo $content;
 	?>
 <?php
 	//footer of template
-	$this->load->view('layout/footer')
+	$this->load->view('layout/front_footer')
 ?>
